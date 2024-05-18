a0bb3774044f12dfb1ab4a777280c82b00101754
Maciej Słowiak <slowiakmaciej@gmail.com>
2024-05-18 10:20:11 +0200

    POC init

---
+++ stream-gathers/src/main/java/com/streamgathers/StreamGathersApplication.java
@@ -0,0 +1,13 @@
+package com.streamgathers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamGathersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamGathersApplication.class, args);
+	}
+
+}
---
+++ stream-gathers/src/test/java/com/streamgathers/StreamGathersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamgathers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamGathersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
